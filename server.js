1e0861516df14779d4c703912c9a2be5772fa637
Vendler Ákos <73746378+AkosVendler@users.noreply.github.com>
2025-09-18 17:25:49 +0200

    Add files via upload

--- server.js
+++ server.js
@@ -2,12 +2,15 @@
 const express = require("express");
 const http = require("http");
 const { Server } = require("socket.io");
+const path = require("path");
 
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: { origin: "*" }
 });
+
+app.use(express.static(path.join(__dirname, "public")));
 
 // egyszerű memória tároló lobbykra
 const lobbies = {};
