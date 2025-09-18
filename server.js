eac40ed8a4173159120805f3a10a1c5873de4fbd
Vendler Ákos <73746378+AkosVendler@users.noreply.github.com>
2025-09-18 18:05:00 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,19 +1,13 @@
-// server.js
 const express = require("express");
 const http = require("http");
 const { Server } = require("socket.io");
-const path = require("path");
 
 const app = express();
 const server = http.createServer(app);
-const io = new Server(server, {
-  cors: {
-    origin: "*",
-    methods: ["GET","POST"]
-  }
-});
 
-app.use(express.static(path.join(__dirname, "public")));
+const io = new Server(server, {
+  cors: { origin: "*", methods: ["GET", "POST"] }
+});
 
 // memória tároló lobbykra
 const lobbies = {};
@@ -21,50 +15,52 @@ const lobbies = {};
 io.on("connection", (socket) => {
   console.log("User connected:", socket.id);
 
-  socket.on("joinLobby", (lobbyCode, username) => {
+  // csatlakozás egy lobbyhoz
+  socket.on("joinLobby", ({ lobbyCode, username }) => {
     socket.join(lobbyCode);
+    socket.lobbyCode = lobbyCode;
+    socket.username = username;
 
     if (!lobbies[lobbyCode]) {
-      lobbies[lobbyCode] = {
-        users: [],
-        counter: 0
-      };
+      lobbies[lobbyCode] = { users: [], counter: 0 };
     }
 
-    // ellenőrizzük, hogy nincs-e már benne a felhasználó
-    if (!lobbies[lobbyCode].users.some(u => u.id === socket.id)) {
+    // ha még nincs benne, adjuk hozzá
+    if (!lobbies[lobbyCode].users.find(u => u.id === socket.id)) {
       lobbies[lobbyCode].users.push({ id: socket.id, name: username });
     }
 
-    // minden kliens frissítése a lobbyban
+    // mindenki frissítése a lobbyban
     io.to(lobbyCode).emit("lobbyUpdate", {
       users: lobbies[lobbyCode].users.map(u => u.name),
       counter: lobbies[lobbyCode].counter
     });
   });
 
-  socket.on("incrementCounter", (lobbyCode) => {
-    if (lobbies[lobbyCode]) {
-      lobbies[lobbyCode].counter++;
+  // számláló növelése
+  socket.on("incrementCounter", () => {
+    const lobbyCode = socket.lobbyCode;
+    if (!lobbyCode || !lobbies[lobbyCode]) return;
+
+    lobbies[lobbyCode].counter++;
+    io.to(lobbyCode).emit("lobbyUpdate", {
+      users: lobbies[lobbyCode].users.map(u => u.name),
+      counter: lobbies[lobbyCode].counter
+    });
+  });
+
+  // kilépés
+  socket.on("disconnect", () => {
+    const lobbyCode = socket.lobbyCode;
+    if (lobbyCode && lobbies[lobbyCode]) {
+      lobbies[lobbyCode].users = lobbies[lobbyCode].users.filter(u => u.id !== socket.id);
       io.to(lobbyCode).emit("lobbyUpdate", {
         users: lobbies[lobbyCode].users.map(u => u.name),
         counter: lobbies[lobbyCode].counter
-      });
-    }
-  });
-
-  socket.on("disconnect", () => {
-    for (const [code, lobby] of Object.entries(lobbies)) {
-      lobby.users = lobby.users.filter(u => u.id !== socket.id);
-      io.to(code).emit("lobbyUpdate", {
-        users: lobby.users.map(u => u.name),
-        counter: lobby.counter
       });
     }
     console.log("User disconnected:", socket.id);
   });
 });
 
-server.listen(3001, () => {
-  console.log("Server running on http://localhost:3001");
-});
+server.listen(3001, () => console.log("Server running on http://localhost:3001"));
